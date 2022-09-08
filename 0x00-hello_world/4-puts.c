c606c34351751ec04d52b93a3e4223ff3fbdad59
Mohammed Doti <mamodoti@gmail.com>
2022-09-08 14:52:40 -0700

    Task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,19 @@
 
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ *  * main - Entry point
  *
- *   * Return: 0 if success.
+ *   *
  *
- *    */
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 
