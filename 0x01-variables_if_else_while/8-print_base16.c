ec89687b265312c134bf48bacc405cf42f76bc7b
Mohammed Doti <mamodoti@gmail.com>
2022-09-09 13:28:54 -0700

    Task 8 commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 /**
-   * main -Entry point
-    * Return: Always 0 (Success)
-     */
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
 int main(void)
 
 {
-		char c = 'a';
+	int z;
 
-		int num;
-
-		for (num = 0; num < 16; num++)
-		{
-		if (num < 10)
-		}
-		putchar(num + '0');
-
-		} else
-		{
-																										putchar(c);
-																										c++;
-																														}
-		}
-		putchar('\n');
-		return (0);
+	for (z = 48; z <= 57; z++)
+	{
+		putchar(z);
+	}
+	for (z = 97; z <= 102; z++)
+	{
+		putchar(z)
+	}
+	putchar('\n');
+	return (0);
 }
